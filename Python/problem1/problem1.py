dd1ac7eba2193fba6e44b51a6d87d15c8abad8e2
Nicholas Card <nick2253@gmail.com>
2012-08-07 15:38:51 -0700

    Problem 1 v2

--- Python/problem1/problem1.py
+++ Python/problem1/problem1.py
@@ -25,13 +25,19 @@ def sumHelper(num, maxIndex):
                 x_maxIndex = num*maxIndex solved recursively
     '''
     
-    if maxIndex < 0
+    if maxIndex < 0:
         return ["error", "error"]
-    elif maxIndex = 0
+    elif maxIndex == 0:
         return [0,0]
-    elif maxIndex = 1
+    elif maxIndex == 1:
         return [num, 0]
-    else
+    else:
         [newSum,numAtLowerIndex] = sumHelper(num,maxIndex-1)
         numAtIndex = num + numAtLowerIndex
         return [numAtIndex, numAtIndex + newSum]
+
+if __name__ == "__main__":
+    print "Sumhelper(10,0) = " + str(sumHelper(10,0))
+    print "Sumhelper(10,0) = " + str(sumHelper(10,2))
+    print "Sumhelper(3,3) = " + str(sumHelper(3,3))
+    print "SumOfMultiples([10],11) = " + str(sumOfMultiples([10],11)) 
