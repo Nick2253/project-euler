6e68e45f0a26d7119cfbaad6c3687487ba60c4e7
Nick2253 <nick2253@gmail.com>
2012-08-07 16:06:41 -0700

    Update Python/problem1/problem1.py
    The "fast" way doesn't work correctly.  It counts numbers that are multiples of more than one number twice or more.

--- Python/problem1/problem1.py
+++ Python/problem1/problem1.py
@@ -12,6 +12,7 @@ def sumOfMultiples(numList,maxNum):
         maxIndex = (maxNum-1)/num
         sumTotal += sumHelper(num,maxIndex)[0]
         
+    
     return sumTotal
         
 def sumHelper(num, maxIndex):
@@ -36,9 +37,25 @@ def sumHelper(num, maxIndex):
         numAtIndex = num + numAtLowerIndex
         return [numAtIndex + newSum, numAtIndex]
 
+def sumOfMultiplesEasy():
+    total = 0
+    a = 0
+    while a<1000:
+        if (a%5==0 or a%3 == 0):
+            total += a
+        a += 1
+    return total
+
 if __name__ == "__main__":
-    //Test Functions//
-    print "Sumhelper(10,0) = " + str(sumHelper(10,0))
-    print "Sumhelper(10,2) = " + str(sumHelper(10,2))
-    print "Sumhelper(3,3) = " + str(sumHelper(3,3))
-    print "SumOfMultiples([3,5],1000) = " + str(sumOfMultiples([3,5],1000)) 
+    #print "Sumhelper(10,0) = " + str(sumHelper(10,0)) #[0,0]
+    #print "Sumhelper(3,1) = " + str(sumHelper(3,1)) #[3,3]
+    #print "Sumhelper(10,2) = " + str(sumHelper(10,2)) #[30,20]
+    #print "Sumhelper(3,3) = " + str(sumHelper(3,3)) #[18,9]
+    #print "Sumhelper(5,5) = " + str(sumHelper(5,5)) #[75,25]
+    #print "SumOfMultiples([3,5],10) = " + str(sumOfMultiples([3,5],10)) #23
+    #print "SumOfMultiples([3],1000) = " + str(sumOfMultiples([3],1000))
+    print "SumOfMultiples([5],1000) = " + str(sumOfMultiples([5],1000))
+    #fast(?) way#
+    print "SumOfMultiples([3,5],1000) = " + str(sumOfMultiples([3,5],1000))
+    #slow(?) way#
+    print "SumOfMultiplesEasy() = " + str(sumOfMultiplesEasy())
