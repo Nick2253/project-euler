67023309662537f46d4a448113b6fda8cec3144e
Nick2253 <nick2253@gmail.com>
2012-08-08 13:02:39 -0700

    Update Python/euler006/euler006.py

--- Python/euler006/euler006.py
+++ Python/euler006/euler006.py
@@ -1,43 +1,35 @@
-def sumOfMultiples(numList,maxNum):
-    '''Determines the sum of all the multiples of the numbers in numList less than maxNum
+def sumOfSquares(maxNum):
+    '''Determines the sum of all the square of the natural numbers less than maxNum
         input:
-            numList = list of ints
             maxNum = int
         output:
-            sumTotal = int sum of all multiples of numbers in numList less than maxNum
+            sumTotal = int sum of all squares
     '''
     sumTotal = 0
     
-    for num in numList:
-        maxIndex = (maxNum-1)/num
-        sumTotal += sumHelper(num,maxIndex)[0]
+    for num in range(1,maxNum+1):
+        sumTotal += num**2
         
     return sumTotal
         
-def sumHelper(num, maxIndex):
-    '''Determines the sum of all the multiples of num up to num*maxIndex
+def squareOfSum(maxNum):
+    '''Determines the square of the sum of the natural numbers less than maxNum
         input:
-            num = int
-            maxIndex = int
+            maxNum = int
         output:
-            [sum_maxIndex, x_maxIndex]
-                sum_maxIndex = sum of all the multiples of num up to num*maxIndex
-                x_maxIndex = num*maxIndex solved recursively
+            sumTotal = int square of sum
     '''
     
-    if maxIndex < 0:
-        return ["error", "error"]
-    elif maxIndex == 0:
-        return [0,0]
-    elif maxIndex == 1:
-        return [num, 0]
-    else:
-        [newSum,numAtLowerIndex] = sumHelper(num,maxIndex-1)
-        numAtIndex = num + numAtLowerIndex
-        return [numAtIndex, numAtIndex + newSum]
+    sumTotal = 0
+    
+    for num in range(1,maxNum+1):
+        sumTotal += num
+        
+    return sumTotal**2
+
+def sumDiff(maxNum):
+    return squareOfSum(maxNum) - sumOfSquares(maxNum)
 
 if __name__ == "__main__":
-    print "Sumhelper(10,0) = " + str(sumHelper(10,0))
-    print "Sumhelper(10,0) = " + str(sumHelper(10,2))
-    print "Sumhelper(3,3) = " + str(sumHelper(3,3))
-    print "SumOfMultiples([10],11) = " + str(sumOfMultiples([10],11)) 
+    print "sumDiff(10) = " + str(sumDiff(10)) #2640
+    print "sumDiff(100) = " + str(sumDiff(100)) #??
