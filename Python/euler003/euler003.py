c110c22cfff7ecfd5fd07114fc4da20a42c849dc
Nick2253 <nick2253@gmail.com>
2012-08-07 17:25:12 -0700

    Update Python/euler003/euler003.py

--- Python/euler003/euler003.py
+++ Python/euler003/euler003.py
@@ -1,43 +1,35 @@
-def sumOfMultiples(numList,maxNum):
-    '''Determines the sum of all the multiples of the numbers in numList less than maxNum
-        input:
-            numList = list of ints
-            maxNum = int
-        output:
-            sumTotal = int sum of all multiples of numbers in numList less than maxNum
-    '''
-    sumTotal = 0
-    
-    for num in numList:
-        maxIndex = (maxNum-1)/num
-        sumTotal += sumHelper(num,maxIndex)[0]
-        
-    return sumTotal
-        
-def sumHelper(num, maxIndex):
-    '''Determines the sum of all the multiples of num up to num*maxIndex
-        input:
-            num = int
-            maxIndex = int
-        output:
-            [sum_maxIndex, x_maxIndex]
-                sum_maxIndex = sum of all the multiples of num up to num*maxIndex
-                x_maxIndex = num*maxIndex solved recursively
-    '''
-    
-    if maxIndex < 0:
-        return ["error", "error"]
-    elif maxIndex == 0:
-        return [0,0]
-    elif maxIndex == 1:
-        return [num, 0]
-    else:
-        [newSum,numAtLowerIndex] = sumHelper(num,maxIndex-1)
-        numAtIndex = num + numAtLowerIndex
-        return [numAtIndex, numAtIndex + newSum]
+import math
 
+def largestPrimeFactor(x):
+    '''Determines the largest prime factor of number x
+        input:
+            x = int
+        output:
+            lpf = largest prime factor of x
+    '''
+    
+    if x == 1:
+        return 1
+    elif x == 0:
+        return 0
+    
+    lpf = 0
+    i = 2
+    isPrime = True
+    
+    while i < math.sqrt(x):
+        if x%i == 0:
+            isPrime = False
+            lpf = max([lpf,largestPrimeFactor(i),largestPrimeFactor(x/i)])
+        i += 1
+        
+    if isPrime:
+        lpf = x
+        
+    return lpf
+        
 if __name__ == "__main__":
-    print "Sumhelper(10,0) = " + str(sumHelper(10,0))
-    print "Sumhelper(10,0) = " + str(sumHelper(10,2))
-    print "Sumhelper(3,3) = " + str(sumHelper(3,3))
-    print "SumOfMultiples([10],11) = " + str(sumOfMultiples([10],11)) 
+    print "lpf(10) = " + str(largestPrimeFactor(10)) #5
+    print "lpf(46) = " + str(largestPrimeFactor(46)) #23
+    print "lpf(13195) = " + str(largestPrimeFactor(13195)) #29
+    print "lpf(600851475143) = " + str(largestPrimeFactor(600851475143)) #??
